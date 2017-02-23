28d709b539031225d4aeb4ea60a90d1b39446014
Étienne Oz <salut@etienneozeray.fr>
2017-02-23 18:34:35 +0100

    nettoyage de code

--- index.php
+++ index.php
@@ -3,55 +3,49 @@
   <head lang="fr-FR">
     <meta charset="UTF-8">
     <?php
-      ///////////////////////////////
-      // Here goes the page title: //
-      ///////////////////////////////
-      $theTitle = 'http://etienneozeray.fr/UrlDropper';
-      echo '<title>'.$theTitle.'</title>';
+      // Here goes the page title:
+      $pageTitle = 'http://etienneozeray.fr/UrlDropper';
     ?>
-    <link type="text/css" rel="stylesheet" href="style.css" />
+    <title><?= $pageTitle ?></title>';
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
   </head>
   <body>
-    <?php
-      echo '<div class="header">
-        <h1><a href="'.$theTitle.'">'.$theTitle.'</a></h1>';
-        ?>
+
+    <header>
+    <h1><a href="<?= $theTitle ?>"><?= $theTitle ?></a></h1>'
         <span class="title toggleInfos">All you need is links.</span></p>
-    </div>
+    </header>
+
     <div class="content">
       <button class="moreInfos">Need more informations?</button>
       <button class="lessInfos">Don't care</button>
       <button class="moreCredits">WTF?</button>
       <button class="lessCredits">Who care?</button>
       <?php
-        ///////////////////////////
-        // Here goes your feeds: //
-        ///////////////////////////
-        $file = "http://etienneozeray.fr/shaarli/?do=rss&searchtags=DropIt";
-        $content = file_get_contents($file);
-        $x = new SimpleXmlElement($content);
-        echo "<ul class='liste'>";
-
-        foreach($x->channel->item as $entry) {
-          echo "<li class='theUrl'>";
-          echo "  <a href='$entry->link'>" . $entry->link . "</a>";
-          echo "  <span class='toggleInfos'>";
-          echo "    <span class='title'>" . $entry->title . " </span>";
+        // Here goes your feeds:
+        $feed = "http://etienneozeray.fr/shaarli/?do=rss&searchtags=DropIt";
+        $content = file_get_contents($feed);
+        $xml = new SimpleXmlElement($content);
+        ?>
+        <ul class='liste'>
+        <?php
+        foreach($xml->channel->item as $entry) {
           $categories = $entry->category;
-            $nb = count($categories);
-            for ($i=0; $i<$nb; $i++){
-              echo '<span class="'.$entry->title.' categories">' .$categories[$i]. '</span>';
-            }
-          echo " <sup class='date'>[" . $entry->pubDate . "]</sup>";
-          echo "</li>";
-        }
-        echo "</ul>";
-
-      ?>
+          $nb = count($categories);
+          ?>
+          <li class='theUrl'>
+            <a href='<?= $entry->link ?>'><?= $entry->link ?></a>
+            <span class='toggleInfos'>
+              <span class='title'></span>
+            <?php for ($i=0; $i<$nb; $i++){ ?>
+              <span class="<?= $entry->title ?> categories"><?= $categories[$i] ?></span>
+            <?php } ?>
+           <sup class='date'>[<?= $entry->pubDate ?>]</sup>
+          </li>
+        <?php } ?>
+        </ul>
     </div>
-    <!--————
-    Credits:
-    —————-->
+
     <div class="footer">
       Build by <a href="http://github.com/EtienneOz">ÉtienneOz</a>.<br/>
       Generated with my <a href="http://sebsauvage.net/wiki/doku.php?id=php:shaarli">Shaarli</a> feeds.<br/>
